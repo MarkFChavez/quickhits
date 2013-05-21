bf8f5ca33cb7aa94f9229c1093a4eab792659499
Mark <los_populares@yahoo.com>
2013-05-21 19:22:14 +0800

    init commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Quickhits::Application.config.secret_token = '553c483f13c95a3359fd8d57e46b1330327670cf25b6b1c0c621a6500f694e449d5476d161b0c16cb194b089ab7a4f8b712ffbf92c6b5afdc5544e568201197a'
